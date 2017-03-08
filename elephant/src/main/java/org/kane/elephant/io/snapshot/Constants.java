69f3ee5418502e5033e8efc363a04462d2083bd4
jim.kane@gmail.com <kMCv2$4QdozwAG>
2017-03-08 15:11:38 -0700

    Initial code split from org.kane.base

---
+++ elephant/src/main/java/org/kane/elephant/io/snapshot/Constants.java
@@ -0,0 +1,6 @@
+package org.kane.elephant.io.snapshot;
+
+public class Constants
+{
+	static public final long MAXIMUM_OBJECT_SIZE = 1024l*1024l; // 1 Megabye
+}
---
+++ elephant/src/main/java/org/kane/elephant/io/snapshot/OperationUploadSnapshot.java
@@ -0,0 +1,55 @@
+package org.kane.elephant.io.snapshot;
+
+import org.kane.base.threading.OperationRunnable;
+import org.kane.elephant.io.benchmark.AWSAPIKeys;
+
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+
+public class OperationUploadSnapshot extends OperationRunnable
+{
+	private OperationSnapshot snapshot_operation;
+	private Upload my_upload;
+	
+	public OperationUploadSnapshot(OperationSnapshot snapshot_operation)
+	{
+		this.snapshot_operation = snapshot_operation;
+	}
+
+	
+	protected Result performOperation() throws Exception
+	{
+		TransferManager manager = new TransferManager(AWSAPIKeys.getAWSCredentialsDev()); 
+
+		SnapshotRequest request = snapshot_operation.getSimpleRequest();
+		
+		PutObjectRequest put_request = new PutObjectRequest( 
+				request.getSimpleDestinationBucketName(), 
+				request.getSimpleDestinationKey(), 
+				snapshot_operation.getSimpleDestination()
+			);
+		
+		
+		my_upload = manager.upload(put_request);
+
+		while(true)
+		{
+			if ( my_upload.isDone() ) break;
+			Thread.currentThread().sleep(500);
+		}
+
+		manager.shutdownNow();
+		
+		return Result.SUCCESS;
+	}
+	
+	public long getBytesUploaded()
+	{
+		if ( my_upload != null )
+			 return my_upload.getProgress().getBytesTransfered();
+		
+		return 0;
+	}
+	
+}
